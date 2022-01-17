033b5b05ec968440c5a7b7980e28a2ada5cdf098
Wolf Larsen <60103389+tahaflz123@users.noreply.github.com>
2022-01-17 14:12:07 +0300

    removed an import

--- src/test/java/com/basketball/test/BasketballApplicationTests.java
+++ src/test/java/com/basketball/test/BasketballApplicationTests.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
